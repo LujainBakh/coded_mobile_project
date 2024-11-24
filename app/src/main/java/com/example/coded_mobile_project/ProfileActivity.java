ba3061a01948b277a41ca9a21288492ceec29aa4
lujainbakh <lujainbakhurji@gmail.com>
2024-11-24 17:37:29 +0300

    testing

---
+++ app/src/main/java/com/example/coded_mobile_project/ProfileActivity.java
@@ -0,0 +1,48 @@
+package com.example.coded_mobile_project;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_instructors);
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setSelectedItemId(R.id.profile);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.home) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                overridePendingTransition(R.anim.silde_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            } else if (item.getItemId() == R.id.instrcutors) {
+                startActivity(new Intent(getApplicationContext(), InstructorsActivity.class));
+                overridePendingTransition(R.anim.silde_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            } else if (item.getItemId() == R.id.Officehours) {
+                startActivity(new Intent(getApplicationContext(), OfficeHoursActivity.class));
+                overridePendingTransition(R.anim.silde_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            } else if (item.getItemId() == R.id.profile) {
+                return true;
+            }
+            return false;
+        });
+    }
+}
