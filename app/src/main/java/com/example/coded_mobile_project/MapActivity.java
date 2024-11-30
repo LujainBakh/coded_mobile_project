15d7a66f1a861e8a9c5eb2de5139683f6e4073e7
amjad <@sanji88K>
2024-12-01 00:33:18 +0300

    don't mind me just adding some map stuff :)

--- app/src/main/java/com/example/coded_mobile_project/MapActivity.java
+++ app/src/main/java/com/example/coded_mobile_project/MapActivity.java
@@ -147,6 +147,7 @@ public class MapActivity extends AppCompatActivity implements
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 // Permission granted, enable location
